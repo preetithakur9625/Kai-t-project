dc900bc301ca82287def4aa6c88aa6b3a18cf009
preetithakur <bhumirathor48@gmail.com>
2022-10-15 08:53:53 +0530

    updatedCode

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 
+$(document).ready(function () {
+    let rightSidePanel = $("#dashboard-app");
+    let sidebar = $("#sidebar");
+    let hideSideBar = $("#hideSideBar");
+    let showSideBar = $("#showSideBar");
+
+    function hideSideBarFunction() {
+        sidebar.css("margin-left", "-1000px");
+        rightSidePanel.addClass("m-0");
+        showSideBar.show();
+    }
+    function showSideBarFunction() {
+        sidebar.css("margin-left", "0px");
+        rightSidePanel.removeClass("m-0");
+        showSideBar.hide();
+    }
+
+
+
+    hideSideBar.click(hideSideBarFunction);
+    showSideBar.click(showSideBarFunction);
+})
